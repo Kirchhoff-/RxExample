2d9f34339f1dfe81703b52ffa415caf16886d2dd
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-05-11 22:17:46 +0300

    Add first example.

--- app/src/main/java/com/example/kirchhoff/rxexample/MainActivity.java
+++ app/src/main/java/com/example/kirchhoff/rxexample/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.kirchhoff.rxexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.firstExample).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.firstExample:
+                Intent intent = new Intent(this, FirstActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
